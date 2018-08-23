cced5c04e2a3a735a8fcd683cc3c249d589c1826
werrpy <werrpy@gmail.com>
2018-08-23 16:27:39 -0400

    parse labels

--- discogsbb.py
+++ discogsbb.py
@@ -9,7 +9,7 @@ class BBCode:
   def tracklist(self, list):
     bbcode = '[list=1]'
     for track in list:
-      if not track.position or not track.title or not track.duration:
+      if not track.position and not track.title and not track.duration:
         break
       bbcode += '[*]'
       if track.position:
@@ -24,6 +24,27 @@ class BBCode:
 
   def link(self, url):
     return '[url=' + str(url) + ']' + str(url) + '[/url]'
+
+  def link_name(self, url, name):
+    return '[url=' + str(url) + ']' + str(name) + '[/url]'
+
+def parse_label(label):
+  if label.name:
+    if label.urls and len(label.urls) > 0:
+      print(bb.link_name(label.urls[0], label.name), end = '')
+    else:
+      print(label.name, end = '')
+  if label.profile:
+    # split profile by paragraph
+    p = label.profile.split("\n\r")
+    # display first paragraph
+    print(" - " + p[0])
+  else:
+    print("")
+  # more label urls
+#  if label.urls and len(label.urls) > 1:
+#    for url in label.urls:
+#      print(bb.link(url))
 
 # Discogs personal access token
 # https://www.discogs.com/settings/developers
@@ -41,6 +62,9 @@ parser = argparse.ArgumentParser(description='Discogs API')
 parser.add_argument('discogs', type=int, help='Discogs ID')
 args = parser.parse_args()
 
+# BBCode formatter
+bb = BBCode()
+
 # Discogs ID = args.discogs
 release = d.release(args.discogs)
 
@@ -48,19 +72,32 @@ release = d.release(args.discogs)
 artists = ', '.join(str(artist.name) for artist in release.artists)
 
 # artists - title
-title = artists + ' - ' + release.title
+title = artists + ' - ' + release.title + ' (' + str(release.year) + ')'
 print('Title: ' + title)
 
 # comma seperated genres
-tags = ', '.join(release.genres)
-print('Tags: ' + tags)
+if release.genres:
+  tags = ', '.join(release.genres)
+  print('Tags: ' + tags)
 
-# BBCode formatter
-bb = BBCode()
+# country
+if release.country:
+  print('Country: ' + release.country)
+
+# get unique labels
+seen = set()
+labels = [x for x in release.labels if not (x.id in seen or seen.add(x.id))]
+
+# labels
+if len(labels) > 0:
+  print('Labels: ')
+  for lbl in labels:
+    label = d.label(lbl.id)
+    parse_label(label)
 
 # images
-print('Images: ')
 if release.images is not None:
+  print('Images: ')
   for img in release.images:
     print(bb.image(img['uri']))
 
